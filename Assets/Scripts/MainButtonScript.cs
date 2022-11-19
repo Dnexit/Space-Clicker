c0353640d11021e674ee8918105ded2906d27114
Darya Nkhd <dar05ia003@gmail.com>
2022-11-19 12:51:50 +0300

    написали основной скрипт

--- Assets/Scripts/MainButtonScript.cs
+++ Assets/Scripts/MainButtonScript.cs
@@ -1,8 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainButtonScript : MonoBehaviour
 {
-    // Button MainButton 
+    private Button mainButton;
+    [SerializeField]private TextMeshProUGUI scoreText;
+    [SerializeField] private string scoreBaseText = "SCORE: ";
+    [SerializeField] private string scoreFunText;
+    
+    private int count = 0;
+
+    private void Awake()
+    {
+        mainButton = GetComponent<Button>();
+        
+        mainButton.onClick.AddListener(() =>
+        {
+            count++;
+            scoreText.text = scoreBaseText + count;
+            if (count == 10)
+            {
+                scoreText.text = scoreFunText;
+            }
+        });
+    }
 }
